c18c273d9866f0bec423fff9171ed76268614b26
F4CIO <f4cio@f4cio.com>
2017-03-23 22:06:47 +0100

    Intial version presented in video: http://www.f4cio.com/configoverwritter.aspx

---
+++ CraftSynth.ConfigOverwriter.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace CraftSynth.ConfigOverwriter.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Make sure that in another project CraftSynth.ConfigOverwritter > Properties > Debug > Command-line arguments 
+			//point to three Web.config files variants in this project  
+			//and then build, run or debug that another project and observe changes being done on files in this project.			
+		}
+	}
+}
